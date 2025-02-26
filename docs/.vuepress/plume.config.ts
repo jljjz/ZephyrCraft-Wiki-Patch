86d21022c497f9d6fd239a945575dd737d63d698
Ziphyrien <111620796+Ziphyrien@users.noreply.github.com>
2025-02-26 23:58:23 +0800

    Update plume.config.ts

--- docs/.vuepress/plume.config.ts
+++ docs/.vuepress/plume.config.ts
@@ -11,7 +11,7 @@ export default defineThemeConfig({
   appearance: true,  // 配置 深色模式
 
   social: [
-    { icon: 'github', link: '/' },
+    { icon: 'qq', link: 'https://qm.qq.com/q/K91jAZ5fuo' },
   ],
   // navbarSocialInclude: ['github'], // 允许显示在导航栏的 social 社交链接
   // aside: true, // 页内侧边栏， 默认显示在右侧
