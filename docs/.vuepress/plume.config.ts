744c99dfea04d92b6424308c309277437e951285
Ziphyrien <111620796+Ziphyrien@users.noreply.github.com>
2025-02-26 23:34:14 +0800

    Update plume.config.ts

--- docs/.vuepress/plume.config.ts
+++ docs/.vuepress/plume.config.ts
@@ -1,5 +1,4 @@
 import { defineThemeConfig } from 'vuepress-theme-plume'
-import { defineUserConfig } from 'vuepress'
 import { navbar } from './navbar'
 import { notes } from './notes'
 
@@ -18,12 +17,11 @@ export default defineThemeConfig({
   // aside: true, // 页内侧边栏， 默认显示在右侧
   // outline: [2, 3], // 页内大纲， 默认显示 h2, h3
 
-export default defineUserConfig({
-  theme: plumeTheme({
+
     contributors: {
       mode: 'block',
     },
-  })
+
       
   /**
    * 文章版权信息
